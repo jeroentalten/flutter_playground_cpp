b8dd76f4ae45e83e13e582f3595e19bf7f344405
Jeroen Tissink <jeroen.tissink@bdrtg.net>
2025-03-27 15:50:49 +0100

    Initial commit

---
+++ example/android/app/src/main/kotlin/nl/alten/flutterplayground/cpp/flutter_playground_cpp_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package nl.alten.flutterplayground.cpp.flutter_playground_cpp_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ios/Classes/flutter_playground_cpp.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../flutter_playground_cpp.podspec for more information.
+#include "../../src/flutter_playground_cpp.c"
